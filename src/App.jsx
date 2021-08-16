98c63792f831a0028a2ab566265f86ffeed1b2a0
SHU0744 <62059397+SHU0744@users.noreply.github.com>
2021-08-16 17:12:25 +0900

    state化

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
+import { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  // 動的なブロッックをstate化
+  const [incompleteTodos, setincompleteTodos] = useState(["test", "test2"]);
+  const [completeTodos, setcompleteTodos] = useState(["テスト"]);
+  return (
+    <>
+      <div className="input-area">
+        <input type="text" placeholder="入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未入力</p>
+        <ul>
+          {incompleteTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>　削除</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了</p>
+        <ul>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
 };
