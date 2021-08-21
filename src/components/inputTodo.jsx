26efa20ccac574b00f075ee699b92b7cc7aad319
SHU0744 <62059397+SHU0744@users.noreply.github.com>
2021-08-21 16:25:02 +0900

    css in js

--- src/components/inputTodo.jsx
+++ src/components/inputTodo.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+const style = {
+  "background-color": "#c1ffff",
+  width: "400px",
+  height: "30px",
+  "border-radius": "8px",
+  padding: "8px",
+  margin: "8px"
+};
+
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
   return (
-    <div className="input-area">
+    <div style={style}>
       <input
         type="text"
         placeholder="入力"
